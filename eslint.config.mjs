de6f501a7c665f3ffb89403147f40a2fc7919189
Ye Yu <nearjilt@gmail.com>
2025-12-22 20:21:44 +0800

    chore: add lint and prettier configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,96 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import checkFile from 'eslint-plugin-check-file'
+import import_ from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import typescript from 'typescript-eslint'
 
-export default tseslint.config(
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
   {
-    ignores: ['eslint.config.mjs'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+    rules: {
+      eqeqeq: ['error', 'always', { null: 'never' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'sort-imports': ['error', { ignoreDeclarationSort: true, ignoreCase: true }],
+    },
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+
+  ...typescript.configs.recommended,
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/strict-boolean-expressions': [
+        'error',
+        { allowString: false, allowNumber: false, allowNullableObject: false },
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
     },
   },
-);
+  {
+    files: ['**/*.js'],
+    ...typescript.configs.disableTypeChecked,
+  },
+
+  {
+    plugins: {
+      'check-file': checkFile,
+    },
+    rules: {
+      'check-file/filename-naming-convention': ['error', { '**/*.{js,ts}': '[0-9a-z-.]+' }],
+    },
+  },
+
+  {
+    plugins: {
+      import: import_,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc' },
+          groups: [
+            'type',
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+          ],
+          pathGroups: [{ pattern: '@/**', group: 'internal' }],
+          pathGroupsExcludedImportTypes: [],
+        },
+      ],
+    },
+  },
+
+  prettier,
+
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      'eslint.config.mjs',
+      'prettier.config.mjs',
+      // * The Refer directory may contain other projects, intended for AI usage.
+      'refer',
+    ],
+  },
+]
