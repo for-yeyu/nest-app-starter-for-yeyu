a593cd5f014100ad1ed11b7ec3924a37b1996881
Ye Yu <nearjilt@gmail.com>
2025-12-22 21:46:14 +0800

    chore: 🪛 setup @nestjs/config with base env file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!'
+    const appName = this.configService.get('APP_NAME')
+
+    return `
+      <h1
+        style="
+          margin: 0;
+          margin-bottom: 12px;
+          font-size: 28px;
+          font-weight: 600;
+          color: #212529;
+          font-family: 'Inter', system-ui, -apple-system, BlinkMacSystemFont, sans-serif;
+          letter-spacing: -0.3px;
+        "
+      >
+        Hello~ <br />
+        Your app name is "${appName}" in .env. Do NOT commit .env to public repositories!
+      </h1>
+    `
   }
 }
